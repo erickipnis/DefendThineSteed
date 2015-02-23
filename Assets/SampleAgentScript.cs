ff1700840e9a499638278fc1b2cd47d3e286e5ac
Terrance Muchler <tdm4859@rit.edu>
2015-02-23 15:49:42 -0500

    More NavMesh, took all the code from steering behavior and moved it into sample agent script. added navmesh agent to prefabs

--- Assets/SampleAgentScript.cs
+++ Assets/SampleAgentScript.cs
@@ -5,13 +5,169 @@ public class SampleAgentScript : MonoBehaviour {
 	public Transform target;
 	NavMeshAgent agent;
 	// Use this for initialization
-	void Start () {
-		agent = GetComponent<NavMeshAgent>();
+	Vector3 position;
+	Vector3 velocity;
+	Vector3 acceleration;
+	
+	float wanderAngle = 15.0f;
+	
+	Vector3 targetSeek;
+	
+	float maxSpeed;
+	float maxForce;		
+	
+	// Use this for initialization
+	void Start ()
+	{	
+		agent = GetComponent<NavMeshAgent> ();
+
+		position = transform.position;
+		position.y = 0;
+		
+		velocity = new Vector3(0, 0, 0);
+		acceleration = new Vector3(0, 0, 0);
+		targetSeek = new Vector3(580f, 0f, 845f);
+		
+		maxSpeed = 2;
+		maxForce = 0.1f;		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		agent.SetDestination(target.position);
+	void Update ()
+	{		
 
+		DetermineBehaviors();
+		UpdateForces();
+	}
+	
+	void DetermineBehaviors()
+	{
+		if (gameObject.tag == "Steed")
+		{
+			GameObject[] trolls = GameObject.FindGameObjectsWithTag("Troll");
+			GameObject[] hayBales = GameObject.FindGameObjectsWithTag("Hay");
+			
+			for (int i = 0; i < trolls.Length; i++)
+			{
+				Vector3 trollPosition = trolls[i].transform.position;
+				Vector3 hayPosition = hayBales[i].transform.position;
+				
+				float trollDistance = Vector3.Distance(trollPosition, position);
+				float hayDistance = Vector3.Distance(hayPosition, position);
+				
+				if (trollDistance <= 100)
+				{
+					flee(trollPosition);
+				}
+				//else if (trollDistance > 100 && hayDistance > 100)
+				//{
+				//	wander();
+				//}								
+			}
+		}
+		else if (gameObject.tag == "Troll")
+		{
+			GameObject[] steeds = GameObject.FindGameObjectsWithTag("Steed");
+			
+			for (int i = 0; i < steeds.Length; i++)
+			{
+				Vector3 steedPosition = steeds[i].transform.position;
+				float steedDistance = Vector3.Distance(steedPosition, position);
+				
+				if (steedDistance <= 100)
+				{
+					seek(steedPosition);
+				}
+				else if (steedDistance > 100)
+				{
+					wander();
+				}
+			}
+		}
+	}
+
+	void UpdateForces()
+	{
+		velocity += acceleration;
+		
+		velocity = Vector3.ClampMagnitude(velocity, maxSpeed);
+		
+		position += velocity;
+		
+		transform.position = position;
+		
+		acceleration *= 0;
+	}
+
+	void applyForce(Vector3 force)
+	{
+		acceleration += force;
+		acceleration.y = 0;
+	}
+
+	void seek(Vector3 target)
+	{
+		Vector3 desiredVelocity = target - position;
+		
+		desiredVelocity.Normalize();
+		
+		desiredVelocity *= maxSpeed;
+		
+		Vector3 steerVector = desiredVelocity - velocity;
+		
+		steerVector = Vector3.ClampMagnitude(steerVector, maxForce);
+		
+		applyForce(steerVector);
+	}
+
+	void flee(Vector3 target)
+	{
+		Vector3 desiredVelocity = target - position;
+		
+		desiredVelocity.Normalize();
+		
+		desiredVelocity *= -maxSpeed;
+		
+		Vector3 fleeVector = desiredVelocity - velocity;
+		
+		fleeVector = Vector3.ClampMagnitude(fleeVector, maxForce);
+		
+		applyForce(fleeVector);
+	}
+	
+	void wander()
+	{
+		float circleRadius = 20.0f; 
+		float circleDistance = 20.0f;
+		float angleChange = 0.5f;
+		
+		Vector3 circleCenter = velocity;
+		
+		circleCenter.Normalize();
+		circleCenter *= circleDistance;
+		
+		Vector3 displacement = new Vector3 (0.0f, 0.0f, -1.0f);
+		displacement *= circleRadius;
+		
+		displacement = setAngle(displacement, wanderAngle);
+		
+		float randomNum = Random.Range(0.0f, 1.0f);
+		wanderAngle += randomNum * angleChange - angleChange * .5f;
+		
+		Vector3 wanderForce = circleCenter + displacement;
+		
+		applyForce(wanderForce);
+	}
+	
+	Vector3 setAngle(Vector3 displacement, float angle)
+	{
+		float length = displacement.magnitude;
+		Vector3 directionVector = displacement;
+		
+		directionVector.x = Mathf.Cos(angle) * length;
+		directionVector.z = Mathf.Sin(angle) * length;
+		
+		return directionVector;
 	}
 }
+
