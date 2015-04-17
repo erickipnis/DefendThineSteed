0b7bb38f8bf4e9a1e77fdd30dae043c86e4c3787
Terrance Muchler <tdm4859@rit.edu>
2015-04-17 16:44:12 -0400

    Finished a new controller that doesn't make the character fly all over the place

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	private int level = 0;
+
+	void Awake(){
+		if (instance == null) {
+			instance = this;
+		} 
+		else if (instance != this) {
+			Destroy (gameObject);
+			DontDestroyOnLoad (gameObject);
+			InitGame ();
+		}
+	}
+	// Use this for initialization
+	void InitGame () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/controller.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class controller : MonoBehaviour {
+	private Vector3 moveDirection = Vector3.zero;
+	public float speed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	
+	void Update()
+	{
+		CharacterController controller = GetComponent<CharacterController> ();
+		
+		moveDirection = transform.TransformDirection(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		moveDirection *= speed;
+		
+		controller.Move(moveDirection * Time.deltaTime);
+		//angle = Mathf.Atan2(moveDirection.x, transform.rotation = Quaternion.Euler(new Vector3(0, angle, 0)));
+
+
+	}
+}
